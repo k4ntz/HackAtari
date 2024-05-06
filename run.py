7a6fd9896b0f9cf32609c032fc50399c89167668
Quentin <q.delfosse@gmail.com>
2024-05-06 11:58:28 +0200

    Added Screenshot

--- run.py
+++ run.py
@@ -1,5 +1,13 @@
 from hackatari import HackAtari, HumanPlayable
+import random
+import numpy as np
+import cv2
 
+
+def save_upsampled(rgb_array, k=4, l=4):
+    aug = np.repeat(np.repeat(rgb_array, k, axis=0), l, axis=1)[:,:,[2,1,0]]
+    cv2.imwrite("screenshot.png", aug, [cv2.IMWRITE_PNG_COMPRESSION, 0])
+    print("Screenshot saved as screenshot.png")
 
 if __name__ == "__main__":
     import argparse
@@ -19,6 +27,9 @@ if __name__ == "__main__":
     
     parser.add_argument('-hu', '--human', action='store_true',
                         help='Let user play the game.')
+    
+    parser.add_argument('-p', '--picture', type=int, default=0,
+                        help='Takes a picture after the number of steps provided.')
 
     args = parser.parse_args()
 
@@ -30,9 +41,14 @@ if __name__ == "__main__":
         env.reset()
         done = False
         env.render()
+        nstep = 1
         while not done:
             action = env.action_space.sample()
-            _, _, terminated, truncated, _ = env.step(action)
+            obs, _, terminated, truncated, _ = env.step(action)
             if terminated or truncated:
                 env.reset()
+            if nstep == args.picture:
+                save_upsampled(obs)
+                exit()
+            nstep += 1
         env.close()
