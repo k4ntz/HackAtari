e3e71083f5d7a4c60989d193067dce8248a3e876
BluemlJ <me@jblue.ml>
2024-06-20 17:44:09 +0200

    add a new rf for skiing

---
+++ hackatari/context/skiing/reward_function.py
@@ -0,0 +1,18 @@
+from ocatari.ram.skiing import *
+REWARD=32
+
+def reward_function(self) -> float:
+    global REWARD 
+    ram = self.get_ram()
+    speed = ram[14]*.01
+    score = ram[107]
+    if ram[15] < 4 or ram[15] > 11:
+        reward = speed*0.2
+    else:
+        reward = speed
+    
+    if score != REWARD:
+        reward+=100000
+       
+    REWARD = ram[107]
+    return reward
