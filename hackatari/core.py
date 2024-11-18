608a18a788de8b7fee863d5faecfe407241b72c6
remunds <raban.emunds@gmail.com>
2024-11-18 16:47:14 +0100

    fix compatability to OC_Atari (remove obs_mode)

--- hackatari/core.py
+++ hackatari/core.py
@@ -25,7 +25,7 @@ class HackAtari(OCAtari):
     HackAtari provides variation of Atari Learning Environments. 
     It is built on top of OCAtari, which provides object-centric observations.
     """
-    def __init__(self, env_name: str, modifs=[], switch_modfis=[], switch_frame=1000, rewardfunc_path=None, colorswaps=None, game_mode=0, difficulty=0, obs_mode="dqn", *args, **kwargs):
+    def __init__(self, env_name: str, modifs=[], switch_modfis=[], switch_frame=1000, rewardfunc_path=None, colorswaps=None, game_mode=0, difficulty=0, *args, **kwargs):
         """
         Initialize the game environment.
         """
@@ -35,7 +35,6 @@ class HackAtari(OCAtari):
             self._frameskip = 4
         
         kwargs["frameskip"] = 1
-        kwargs["obs_mode"] = obs_mode
         super().__init__(env_name, *args, **kwargs)
         covered = False
         for cgame in GameList:
