1ec4b1d0245bbe8c39ebe73589e6141a1fc153ec
Quentin Delfosse <q.delfosse@gmail.com>
2025-03-14 17:25:53 +0100

    First stargunner modif

---
+++ hackatari/games/stargunner.py
@@ -0,0 +1,58 @@
+class GameModifications:
+    """
+    Encapsulates game modifications for managing active modifications and applying them.
+    """
+
+    def __init__(self, env):
+        """
+        Initializes the modification handler with the given environment.
+
+        :param env: The game environment to modify.
+        """
+        self.env = env
+        self.active_modifications = set()
+
+    def _set_active_modifications(self, active_modifs):
+        """
+        Specifies which modifications are active.
+
+        :param active_modifs: A list of active modification names.
+        """
+        self.active_modifications = set(active_modifs)
+    
+    def static_mountains(self):
+        """
+        Sets the mountains to static.
+        """
+        ram = self.env.get_ram()
+        print()
+        for i in range(42,70):
+            print(ram[i], end=", ")
+        print()
+        for i, el in enumerate([240, 255, 255, 255, 255, 255, 255, 
+                                126, 60, 255, 249, 240, 224, 192, 
+                                128, 0, 0, 0, 255, 255, 255, 254, 
+                                252, 248, 240, 224, 192, 5]):
+            self.env.set_ram(42+i, el)
+
+    def _fill_modif_lists(self):
+        """
+        Returns the modification lists (step, reset, and post-detection) with active modifications.
+
+        :return: Tuple of step_modifs, reset_modifs, and post_detection_modifs.
+        """
+        modif_mapping = {
+            "static_mountains": self.static_mountains,
+        }
+
+        step_modifs = [modif_mapping[name]
+                       for name in self.active_modifications if name in modif_mapping]
+        reset_modifs = []
+        post_detection_modifs = []
+        return step_modifs, reset_modifs, post_detection_modifs
+
+
+def modif_funcs(env, active_modifs):
+    modifications = GameModifications(env)
+    modifications._set_active_modifications(active_modifs)
+    return modifications._fill_modif_lists()
