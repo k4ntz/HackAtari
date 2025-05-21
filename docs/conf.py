512bbcf6c669e6642030eb21ac7916f908c75353
BluemlJ <bluemlj@gmx.net>
2025-05-21 17:34:20 +0200

    set python to 3.9

--- docs/conf.py
+++ docs/conf.py
@@ -73,7 +73,7 @@ release = ''
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = "python3.13"
+language = "python3.9"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
